934ea2a4fe141a1e473d1a03b5a064bbf90354ac
Axel Bellec <axel.bellec@outlook.fr>
2016-07-02 02:12:45 +0200

    improve environment variables handling

--- app/config_common.py
+++ app/config_common.py
@@ -1,24 +1,33 @@
 import os
 
 from dotenv import load_dotenv
+from app.util import try_keys
 
 load_dotenv('.env')
 
 # Secret key for generating tokens
-SECRET_KEY = os.environ.get('APP_SECRET')
+SECRET_KEY = os.environ.get('APP_SECRET') or 'houdini'
+
+# Postges credentials
+POSTGRES_USER = try_keys(os.environ, ['POSTGRES_USER'], 'postgres')
+POSTGRES_PASS = try_keys(os.environ, ['POSTGRES_PASS'], 'postgres')
+POSTGRES_HOST = try_keys(os.environ, ['POSTGRES_HOST'], 'postgres')
+POSTGRES_PORT = try_keys(os.environ, ['POSTGRES_PORT'], 5433)
+POSTGRES_DBNAME = try_keys(os.environ, ['POSTGRES_DBNAME'], 'openbikes')
 
 # Postgres connexion
 SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pwd}@{host}:{port}/{name}'.format(
-    user=os.environ.get('POSTGRES_USER'),
-    pwd=os.environ.get('POSTGRES_PASS'),
-    host=os.environ.get('POSTGRES_HOST'),
-    port=os.environ.get('POSTGRES_PORT'),
-    name=os.environ.get('POSTGRES_DBNAME')
+    user=POSTGRES_USER,
+    pwd=POSTGRES_PASS,
+    host=POSTGRES_HOST,
+    port=POSTGRES_PORT,
+    name=POSTGRES_DBNAME
 )
-SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-GEOJSON_FOLDER = 'collecting/geojson/'
-REGRESSORS_FOLDER = 'training/regressors/'
+SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS') or True
+
+GEOJSON_FOLDER = try_keys(os.environ, ['GEOJSON_FOLDER'], 'collecting/geojson/')
+REGRESSORS_FOLDER = try_keys(os.environ, ['REGRESSORS_FOLDER'], 'training/regressors/')
 
 TIMEZONE = 'Europe/Paris'
 
