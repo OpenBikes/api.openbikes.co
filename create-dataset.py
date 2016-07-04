c2114e3f659f09248657fbc10705c242dcf0c174
MaxHalford <maxhalford25@gmail.com>
2016-07-04 10:05:49 +0200

    WIP dataset creation

---
+++ create-dataset.py
@@ -0,0 +1,10 @@
+'''
+This script creates a directory named after a city in the same directory where
+this script is launched from. The created directory contains:
+
+- A directory containing a .csv file for each station.
+- A .csv file containing city's weather entries.
+- A .csv file containing the coordinates of each station.
+
+This script has to be run from the root of this repository (next to `run.py`).
+'''
--- scripts/import-dump.py
+++ scripts/import-dump.py
@@ -1,3 +1,16 @@
+'''
+This scripts imports timeseries data and weather data from the remote database
+and dumps it into the local database. This can be factorized for both datasets
+as they have the same top-level structure (their indexes are datetimes).
+
+The script will use the latest date in the local database to know how much data
+to import. If no previous entries exist then all the data will be imported.
+Running the script will basically sync the local database with the remote
+database.
+
+This script can be run from any directory.
+'''
+
 import subprocess
 import time
 import argparse
@@ -8,7 +21,7 @@ from termcolor import colored
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('city', type=str, help='City for which to import data.')
+parser.add_argument('city', type=str, help='City for which to import data')
 parameters = parser.parse_args()
 city = parameters.city
 
@@ -37,26 +50,34 @@ C = {
     }
 }
 
-notify('Established database connections', 'green')
 
-# Check if there already is some data in the local database
-if C['local']['ts'].find().count() != 0:
-    # Get the most recent date in the local database
-    max_date = C['local']['ts'].find_one(sort=[('_id', -1)])['_id']
-    # Delete the latest document to avoid incomplete data
-    C['local']['ts'].delete_one({'_id': max_date})
-    notify('Will only import data for {0} after {1} (included)'.format(city, max_date), 'yellow')
-    # Query the relevant data on the remove server
-    cursor = C['remote']['ts'].find({'_id': {'$gte': max_date}}, sort=[('_id', 1)])
-else:
-    notify('Importing all data for {} (this could take a while)'.format(city), 'yellow')
-    # Query the relevant data on the remove server
-    cursor = C['remote']['ts'].find(sort=[('_id', 1)])
+def fetch(local, remote):
+    notify('Fetching...', 'green')
+    # Check if there already is some data in the local database
+    if local.find().count() != 0:
+        # Get the most recent date in the local database
+        max_date = local.find_one(sort=[('_id', -1)])['_id']
+        # Delete the latest document to avoid incomplete data
+        local.delete_one({'_id': max_date})
+        notify('Will only import data for {0} after {1} (included)'.format(city, max_date), 'yellow')
+        # Query the relevant data on the remove server
+        cursor = remote.find({'_id': {'$gte': max_date}}, sort=[('_id', 1)])
+    else:
+        notify('Importing all data for {} (this could take a while)'.format(city), 'yellow')
+        # Query the relevant data on the remove server
+        cursor = remote.find(sort=[('_id', 1)])
 
-total = cursor.count()
-notify('Fetched {0} document(s)'.format(total), 'cyan')
-# Insert it locally
-for i, c in enumerate(cursor):
-    notify('Inserting document {0} out of {1}'.format(i + 1, total), 'cyan')
-    C['local']['ts'].insert(c)
-notify('Done', 'green')
+    total = cursor.count()
+    notify('Fetched {0} document(s)'.format(total), 'cyan')
+    # Insert it locally
+    for i, c in enumerate(cursor):
+        notify('Inserting document {0} out of {1}'.format(i + 1, total), 'cyan')
+        local.insert(c)
+    notify('Done', 'green')
+
+
+notify('BIKE STATIONS', 'green')
+fetch(C['local']['ts'], C['remote']['ts'])
+
+notify('WEATHER', 'green')
+fetch(C['local']['weather'], C['remote']['weather'])
